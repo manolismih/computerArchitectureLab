538e2f5a26c1cc2335dbf91d9e0d94bea462164d
cpanagiom <57923086+cpanagiom@users.noreply.github.com>
2019-11-22 23:07:56 +0200

    Add files via upload

--- my_prog.c
+++ my_prog.c
@@ -7,8 +7,10 @@ int main()
 	for (int i=2; i<20000; i++)
 		fib[i] = fib[i-1]+fib[i-2];
 	int odd=0;
-	for (int i=0; i<20000; i++)
+	for (int i=0; i<20005; i++)
 		if (fib[i]%2==1) odd++;
+
+	printf("Odd numbers : %d ", odd);
 	return 0;
 }
 
