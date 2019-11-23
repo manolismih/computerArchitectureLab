e03a6e28e1c036d12fc8bc8e6ed0bd4b3778df9d
Emmanouil Michalainas <manolismih@windowslive.com>
2019-11-23 18:12:58 +0200

    [Lab1] Completed assignment for Lab #1

--- my_prog.c
+++ my_prog.c
@@ -7,10 +7,10 @@ int main()
 	for (int i=2; i<20000; i++)
 		fib[i] = fib[i-1]+fib[i-2];
 	int odd=0;
-	for (int i=0; i<20005; i++)
+	for (int i=0; i<20000; i++)
 		if (fib[i]%2==1) odd++;
 
-	printf("Odd numbers : %d ", odd);
+	printf("\nOdd numbers : %d \n", odd);
 	return 0;
 }
 
